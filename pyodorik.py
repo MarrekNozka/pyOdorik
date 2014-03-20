250822c974068ad5671252501daa5fb853c4b512
Tlapička <marek@tlapicka.net>
2014-03-20 12:54:41 +0100

    fix $HOST -> $(hostname)

--- pyodorik.py
+++ pyodorik.py
@@ -3,24 +3,29 @@
 # Soubor:  pyodorik.py
 # Datum:   14.03.2014 11:48
 # Autor:   Marek Nožka, marek <@t> tlapicka <d.t> net
-# Licence: GNU/GPL 
+# Licence: GNU/GPL
 # Úloha:   Jednoduché uživatelské rozhraní pro odorik.
 ############################################################################
 from __future__ import unicode_literals
 
-import sys, os, httplib, urllib
+import sys
+import os
+import subprocess
+import httplib
+import urllib
 import simplejson as json
-from sys import stdin, stdout, stderr
+from sys import stdout, stderr
 ############################################################################
 
-root='/api/v1'
-conffile=os.getenv('HOME')+'/.config/pyodorik.json'
+root = '/api/v1'
+conffile = os.getenv('HOME')+'/.config/pyodorik.json'
+
 
 def printHelp():
     print """pyOdorik
 =====================
 
-Konfigurace je uložene v ~/.config/pyodorik.json 
+Konfigurace je uložene v ~/.config/pyodorik.json
 
 Seznam kontaktů
 ----------------
@@ -38,10 +43,10 @@ Zpětné volání
 $ pyodorik call 756123658
 $ pyodorik call 123456789 756123658
 
-Pokud je zadáno jen jednočíslo na bere se číslo telefonu z kterého 
+Pokud je zadáno jen jednočíslo na bere se číslo telefonu z kterého
 chci volat z konfiguračního souboru.
 
-V konfiguračním souboru si pro každého hostitele můžu nastavit jiné telefoní 
+V konfiguračním souboru si pro každého hostitele můžu nastavit jiné telefoní
 číslo.
 
 
@@ -53,38 +58,39 @@ $ pyodorik help
 
 """
 
+
 def getAuth():
     """Funkce získá uživatelské jméno a heslo z konfiguračního souboru
     """
     try:
-        conf=open( conffile, 'r' )
+        conf = open(conffile, 'r')
     except:
         stderr.write("Nemůžu otvřít soubor {}\n\n".format(conffile))
         sys.exit(10)
     try:
-        auth=json.load(conf)['auth']
+        auth = json.load(conf)['auth']
     except:
         stderr.write("V soubor {} je syntaktická chyba.\n\n".format(conffile))
         sys.exit(20)
     conf.close()
-    
+
     return auth
 
 
-def getFromAPI(method,URL,**kwargs):
-    auth=getAuth()
+def getFromAPI(method, URL, **kwargs):
+    auth = getAuth()
     auth.update(kwargs)
     params = urllib.urlencode(auth)
     try:
-        conn=httplib.HTTPSConnection("www.odorik.cz")
+        conn = httplib.HTTPSConnection("www.odorik.cz")
     except:
         stderr.write("chyba sítě.\n\n")
         sys.exit(30)
-    if method=='POST':    
-        conn.request('POST',root+URL,params)
-    elif method=='GET':
-        conn.request('GET',root+URL+'?'+params)
-    else :
+    if method == 'POST':
+        conn.request('POST', root+URL, params)
+    elif method == 'GET':
+        conn.request('GET', root+URL+'?'+params)
+    else:
         return False
     response = conn.getresponse()
     print response.status, response.reason
@@ -93,55 +99,53 @@ def getFromAPI(method,URL,**kwargs):
     return data
 
 ##############################################################################
-if len(sys.argv) == 1 or sys.argv[1]=='help':
+if len(sys.argv) == 1 or sys.argv[1] == 'help':
     printHelp()
     sys.exit(0)
-elif sys.argv[1]=='list':
-    contacts = getFromAPI("GET" , '/speed_dials.json')
+elif sys.argv[1] == 'list':
+    contacts = getFromAPI("GET", '/speed_dials.json')
     contacts = json.loads(contacts)
     for k in contacts:
         print k['shortcut'], k['name']
     sys.exit(0)
-elif sys.argv[1]=='credit':
-    print 'Kredit: '+getFromAPI("GET" , '/balance') + " korun českých."
-    sys.exit(0) 
-elif sys.argv[1]=='call':
+elif sys.argv[1] == 'credit':
+    print 'Kredit: '+getFromAPI("GET", '/balance') + " korun českých."
+    sys.exit(0)
+elif sys.argv[1] == 'call':
     if len(sys.argv) == 4:
         caller = sys.argv[2]
         recipient = sys.argv[3]
     elif len(sys.argv) == 3:
         recipient = sys.argv[2]
-
         try:
-            conf=open( conffile, 'r' )
+            conf = open(conffile, 'r')
         except:
             stderr.write("Nemůžu otvřít soubor {}\n\n".format(conffile))
             sys.exit(10)
         try:
-            default=json.load(conf)['default']
+            default = json.load(conf)['default']
         except:
-            stderr.write("V soubor {} je syntaktická chyba.\n\n".format(conffile))
+            stderr.write("V soubor {} je syntaktická"
+                         "chyba.\n\n".format(conffile))
             sys.exit(20)
         conf.close()
         try:
-            caller=default[os.getenv('HOST')]
+            caller = default[subprocess.check_output(["hostname"]).strip()]
         except:
-            caller=default['default']
+            caller = default['default']
     else:
         stderr.write('Zadejí číslo odkud voláš a kam\n\n')
         sys.exit(50)
-
     print "Volám z čísla "+caller
     print "     na číslo "+recipient
-    print getFromAPI("POST" , '/callback', caller=caller, recipient=recipient)
-    sys.exit(0) 
-
+    print getFromAPI("POST", '/callback', caller=caller, recipient=recipient)
+    sys.exit(0)
 
 sys.exit(0)
 
 while True:
     try:
-        line = raw_input('app>> ')    
+        line = raw_input('app>> ')
     except EOFError:
         exit(0)
     except KeyboardInterrupt:
