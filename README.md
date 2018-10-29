581ba0e939761c579ee1c90a05efcd75f45035a9
Tlapička <marek@tlapicka.net>
2018-10-29 13:01:54 +0100

    -> OOP
    * změna rozhraní příkazové řádky
    * přidána podpora vyhledávání v adresáři

--- README.md
+++ README.md
@@ -1,44 +1,68 @@
 pyOdorik
-========
+=====================
 
 [Odorik]: http://www.odorik.cz
-
 
 Uživatelská aplikace pro [Odorik][] VoIP.
 
 Use aplication for [Odorik][] VoIP.
 
-Konfigurace
---------------
-
-Konfigurace je uložene v `~/.config/pyodorik.json`
+Konfigurace je uložena v [~/.config/pyodorik.ini](pyodorik.ini).
 
 Seznam kontaktů
 ----------------
 
     $ pyodorik list
+    $ pyodoril search karel
+
 
 Kredit
 -------
 
+    $ pyodorik
     $ pyodorik credit
+
 
 Zpětné volání
 --------------
 
-    $ pyodorik call 756 123 658
-    $ pyodorik call 123 456 789 - 756 123 658
+    $ pyodorik 756 123 658
+    $ pyodorik tonda
+    $ pyodorik 123 456789 - 756123456
 
-Pokud je zadáno jen jedno číslo bere se číslo telefonu z kterého 
-chci volat z konfiguračního souboru.
+
+Pokud je zadáno jen jedno číslo, bere se číslo telefonu z kterého
+chci volat z konfiguračního souboru. Na mezerách nezáleží. Při zadání dvou
+čísel je oddělovačem pomlčka. Dvě telefoní čísla se zadávají v pořadí
+<odkud> - <kam>
 
 V konfiguračním souboru si pro každého hostitele můžu nastavit jiné výchozí
 telefonní číslo.
+
+Pokud není uvedeno telefonní číslo, začne se vyhledávat řetězec v kontaktech.
+Hledání probíhá bez diakritiky.
+
+
+Výpis a volba linky
+--------------
+
+    $ pyodolik lines
+
+Volba linky se provádí pojmenování příkazu, kterým je program volán a sekcí
+[lines] v konfiguračním souboru.
+
+    $ cd ~/bin
+    ~/bin $ ln -s pyodorik.py pyodolik
+    ~/bin $ ln -s pyodorik.py pyodolik.home
+    ~/bin $ ln -s pyodorik.py pyodolik.adam
+    ~/bin $ ln -s pyodorik.py pyodolik.eva
+
+    $ pyodorik.adam 777 555 444
 
 
 Help
 -------
 
-    $ pyodorik
     $ pyodorik help
+
 
--- pyodorik.py
+++ pyodorik.py
@@ -17,69 +17,35 @@ import simplejson as json
 import configparser
 from sys import stdout, stderr
 import os.path
+import re
+import unicodedata
+from os.path import basename
 ############################################################################
-root = '/api/v1'
-conffile = os.getenv('HOME')+'/.config/pyodorik.ini'
 
 
-def printHelp():
-    print("""pyOdorik
-=====================
+class PyOdorik:
+    root = '/api/v1'
+    conffile = os.getenv('HOME')+'/.config/pyodorik.ini'
+    hostname = subprocess.check_output(["hostname"]).strip().decode('ascii')
 
-Konfigurace je uložene v ~/.config/pyodorik.ini
-
-Seznam kontaktů
-----------------
-
-$ pyodorik list
-
-Kredit
--------
-
-$ pyodorik credit
-
-Zpětné volání
---------------
-
-$ pyodorik call 756 123 658
-$ pyodorik call 123 456 789 - 756 123 658
-
-Pokud je zadáno jen jedno číslo na bere se číslo telefonu z kterého
-chci volat z konfiguračního souboru.
-
-V konfiguračním souboru si pro každého hostitele můžu nastavit jiné výchozí
-telefoní číslo.
-
-
-Help
--------
-
-$ pyodorik
-$ pyodorik help
-
-""")
-
-
-def getAuth():
-    '''
-    Funkce získá uživatelské jméno a heslo z konfiguračního souboru
-    '''
-    if not os.path.isfile(conffile):
-        stderr.write('''
+    def __init__(self):
+        if not os.path.isfile(self.conffile):
+            stderr.write('''
 Konfigurační soubor {} neexistuje!
 Příklad konfiguračního souboru najdeš na adrese:
 https://github.com/MarrekNozka/pyOdorik/blob/master/pyodorik.ini
 
-'''.format(conffile))
-        sys.exit(10)
+'''.format(self.conffile))
+            sys.exit(10)
 
-    config = configparser.ConfigParser()
-    config.read(conffile)
-    if 'auth' in config and \
-            'user' in config['auth'] and 'password' in config['auth']:
-        return config['auth']
-    else:
-        stderr.write('''
+        self.config = configparser.ConfigParser()
+        self.config.read(self.conffile)
+        if 'auth' in self.config and \
+                'user' in self.config['auth'] and \
+                'password' in self.config['auth']:
+            self.auth = self.config['auth']
+        else:
+            stderr.write('''
 V konfiguračním souboru {} je třeba sekce
 
 [auth]
@@ -89,87 +55,241 @@ user = abc123456
 Příklad konfiguračního souboru najdeš na adrese:
 https://github.com/MarrekNozka/pyOdorik/blob/master/pyodorik.ini
 
-'''.format(conffile))
-        sys.exit(20)
+'''.format(self.conffile))
+            sys.exit(20)
+        if 'default_caller' in self.config and \
+                self.hostname in self.config['default_caller']:
+            self.caller = self.config['default_caller'][self.hostname]
+        elif 'default_caller' in self.config and \
+                'default' in self.config['default_caller']:
+            self.caller = self.config['default_caller']['default']
+        else:
+            stderr.write('''
+V konfiguračním souboru {0} je třeba sekce
+
+[default_caller]
+default = 123 456 789
+{1} = 5
+
+Příklad konfiguračního souboru najdeš na adrese:
+https://github.com/MarrekNozka/pyOdorik/blob/master/pyodorik.ini
+
+'''.format(self.conffile, self.hostname))
+            sys.exit(21)
+        self.recipient = self.caller
+        if 'lines' in self.config:
+            self.lines = self.config['lines']
+            if 'default' in self.lines:
+                self.line = self.lines['default']
+            else:
+                self.line = None
+            for line in self.lines:
+                '''Pokud je v názvu spustitelného souboru jméno linky,
+                použije se.'''
+                if line in basename(sys.argv[0]):
+                    self.line = self.lines[line]
+        else:
+            self.lines = []
+            self.line = None
+
+    def printHelp():
+        print("""pyOdorik
+=====================
+
+Konfigurace je uložena v ~/.config/pyodorik.ini
+
+Seznam kontaktů
+----------------
+
+$ pyodorik list
+$ pyodoril search karel
 
 
-def getFromAPI(method, URL, **kwargs):
-    auth = getAuth()
-    auth.update(kwargs)
-    params = urllib.parse.urlencode(auth)
-    try:
-        conn = http.client.HTTPSConnection("www.odorik.cz")
-    except:
-        stderr.write("chyba sítě.\n\n")
-        sys.exit(30)
-    if method == 'POST':
-        conn.request('POST', root+URL, params)
-    elif method == 'GET':
-        conn.request('GET', root+URL+'?'+params)
-    else:
-        return False
-    response = conn.getresponse()
-    print(response.status, response.reason)
-    data = response.read()
-    conn.close()
-    return data
+Kredit
+-------
 
+$ pyodorik
+$ pyodorik credit
+
+
+Zpětné volání
+--------------
+
+$ pyodorik 756 123 658
+$ pyodorik 123 456789 - 756123456
+
+$ pyodorik tonda
+
+Pokud je zadáno jen jedno číslo, bere se číslo telefonu z kterého
+chci volat z konfiguračního souboru. Na mezerách nezáleží. Při zadání dvou
+čísel je oddělovačem pomlčka. Dvě telefoní čísla se zadávají v pořadí
+<odkud> - <kam>
+
+V konfiguračním souboru si pro každého hostitele můžu nastavit jiné výchozí
+telefonní číslo.
+
+Pokud není uvedeno telefonní číslo, začne se vyhledávat řetězec v kontaktech.
+Hledání probíhá bez diakritiky.
+
+
+Výpis a volba linky
+--------------
+
+$ pyodolik lines
+
+Volba linky se provádí pojmenování příkazu, kterým je program volán a sekcí
+[lines] v konfiguračním souboru.
+
+$ cd ~/bin
+~/bin $ ln -s pyodorik.py pyodolik
+~/bin $ ln -s pyodorik.py pyodolik.home
+~/bin $ ln -s pyodorik.py pyodolik.adam
+~/bin $ ln -s pyodorik.py pyodolik.eva
+
+Help
+-------
+
+$ pyodorik help
+
+""")
+
+    def getFromAPI(self, method, URL, **kwargs):
+        params = dict(self.auth)
+        params.update(kwargs)
+        params = urllib.parse.urlencode(params)
+        try:
+            conn = http.client.HTTPSConnection("www.odorik.cz")
+        except:
+            stderr.write("chyba sítě.\n\n")
+            sys.exit(30)
+        if method == 'POST':
+            conn.request('POST', self.root + URL, params)
+        elif method == 'GET':
+            conn.request('GET', self.root + URL+'?'+params)
+        else:
+            return False
+        response = conn.getresponse()
+        print(response.status, response.reason)
+        data = response.read()
+        conn.close()
+        return data
+
+    def getCredit(self):
+        return self.getFromAPI("GET", '/balance').decode('ascii')
+
+    def getContacts(self):
+        contacts = self.getFromAPI("GET", '/speed_dials.json')
+        contacts = json.loads(contacts)
+        return contacts
+
+    def getLines(self):
+        lines = self.getFromAPI("GET", '/lines')
+        return lines.decode('ascii').split(',')
+
+    def make(self, args):
+        'zpracuje telefonní čísla na příkazovém řádku'
+        args.pop(0)
+        numbers = ''.join(args).split('-')  # 123 456 789 - 12 555 12 13
+        if not all(numbers):
+            stderr.write('Zadej jedno telefonní číslo \n')
+            stderr.write('... nebo dvě čísla oddělená pomlčkou\n')
+            exit(44)
+        if len(numbers) > 0:
+            self.recipient = numbers.pop()
+        if len(numbers) > 0:
+            self.caller = numbers.pop()
+        if not re.search('^\d+$', self.caller):
+            self.caller = self.findContact(self.caller)
+        if not re.search('^\d+$', self.recipient):
+            self.recipient = self.findContact(self.recipient)
+
+    def findContact(self, exp):
+        'najde kontakty odpovídající výrazu'
+        'ten zmatek s unicodedata a .encode a .decode je tu proto'
+        'aby vyhledávání probíhalo bez diakritiky'
+        def match(item):
+            exppp = unicodedata.normalize('NFD', exp).upper()\
+                  .encode('ascii', 'ignore').decode('ascii')
+            return re.search(exppp,
+                             unicodedata.normalize('NFD', item['name'])
+                             .upper().encode('ascii', 'ignore')
+                             .decode('ascii'),
+                             re.I)
+        print('\nHLEDÁM', exp)
+        calllist = self.getContacts()
+        calllist = list(filter(match, calllist))
+        if len(calllist) == 0:      # nic jsem nenašel
+            return 0
+        elif len(calllist) == 1:    # přesně jeden odpovídá
+            print('    ', calllist[0]['shortcut'],
+                  calllist[0]['name'], calllist[0]['number'])
+            choice = input(' pokračujeme? [A/n]> ')
+            if choice == '' or choice.upper == 'A' or choice.upper == 'Y':
+                return calllist[0]['shortcut']
+            else:
+                return 0
+        else:                       # odpovídajících kontaktů je víc
+            for i, contact in enumerate(calllist, start=1):
+                print(' ', i, ':',
+                      contact['shortcut'], contact['name'], contact['number'])
+            choice = input(' zvol si číslo (0 znamená odchod)[1]> ')
+            if choice == '':
+                return calllist[0]['shortcut']
+            while 1:
+                try:
+                    choice = int(choice)
+                    if choice == 0:
+                        return 0
+                    return calllist[choice-1]['shortcut']
+                except ValueError:
+                    print("CHYBA: Zadej číslo!")
+                except IndexError:
+                    print("CHYBA: Zadej správné číslo!!")
+                choice = input(' zvol si číslo (0 znamená odchod)[1]> ')
+            return
+
+    def call(self):
+        kwargs = {}
+        if self.line:
+            kwargs.update({'line': self.line})
+        if self.caller and self.recipient:
+            return self.getFromAPI("POST", '/callback',
+                                   caller=self.caller,
+                                   recipient=self.recipient,
+                                   **kwargs)
+        else:
+            stderr.write(' Některé z čísel není zadáno správně.\n')
+            exit(51)
 
 ##############################################################################
 
 
 if __name__ == '__main__':
-    if len(sys.argv) == 1 or sys.argv[1] == 'help':
-        printHelp()
-        sys.exit(0)
-    elif sys.argv[1] == 'list':
-        contacts = getFromAPI("GET", '/speed_dials.json')
-        contacts = json.loads(contacts)
-        for k in contacts:
-            print(k['shortcut'], k['name'])
-        sys.exit(0)
-    elif sys.argv[1] == 'credit':
-        print('Kredit: {} Kč'.format(getFromAPI("GET",
-                                                '/balance').decode('ascii')))
-        sys.exit(0)
-    elif sys.argv[1] == 'call':
-        if len(sys.argv) == 4:
-            caller = sys.argv[2]
-            recipient = sys.argv[3]
-        elif len(sys.argv) == 3:
-            recipient = sys.argv[2]
-            try:
-                conf = open(conffile, 'r')
-            except:
-                stderr.write("Nemůžu otvřít soubor {}\n\n".format(conffile))
-                sys.exit(10)
-            try:
-                default = json.load(conf)['default']
-            except:
-                stderr.write("V soubor {} je syntaktická"
-                             "chyba.\n\n".format(conffile))
-                sys.exit(20)
-            conf.close()
-            try:
-                caller = default[subprocess.check_output(["hostname"]).strip()]
-            except:
-                caller = default['default']
-        else:
-            stderr.write('Zadejí číslo odkud voláš a kam\n\n')
-            sys.exit(50)
-        print("Volám z čísla "+caller)
-        print("     na číslo "+recipient)
-        print(getFromAPI("POST", '/callback', caller=caller, recipient=recipient))
-        sys.exit(0)
-
-    sys.exit(0)
-
-while True:
     try:
-        line = input('app>> ')
-    except EOFError:
-        exit(0)
+        pyodorik = PyOdorik()
+
+        if len(sys.argv) == 1 or sys.argv[1] == 'credit':
+            print('Kredit: {} Kč'.format(pyodorik.getCredit()))
+            sys.exit(0)
+        elif sys.argv[1] == 'help':
+            pyodorik.printHelp()
+            sys.exit(0)
+        elif sys.argv[1] == 'lines':
+            print(pyodorik.getLines())
+            sys.exit(0)
+        elif sys.argv[1] == 'list':
+            contacts = pyodorik.getContacts()
+            for k in contacts:
+                print(k['shortcut'], ':', k['name'], ':',  k['number'])
+            sys.exit(0)
+        elif sys.argv[1] == 'search':
+            contacts = pyodorik.findContact(sys.argv[2])
+            sys.exit(0)
+        else:
+            pyodorik.make(sys.argv)
+            stdout.write("\nCaller: {}\n".format(pyodorik.caller))
+            stdout.write("Recipient: {}\n".format(pyodorik.recipient))
+            print(pyodorik.call())
+            sys.exit(0)
     except KeyboardInterrupt:
         exit(1)
-    except:
-        stdout.write("ERROR\n")
